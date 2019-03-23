4c672ef27b145bcab24bc58b47c6f5a67bf6263d
Michael Cain <michaelmcain@me.con>
2019-03-23 14:21:52 -0400

    fixed CSS module import issue

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux'
-import classes from './Layout.css';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, Sidedrawer, Backdrop</div>
         <main className={classes.Content}>
+        {console.log(classes)}
+        {console.log(classes.Content)}
             {props.children}
         </main>
     </Aux>
